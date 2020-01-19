49ded224822cfe969860b8c88665bd3cd6065d76
Aleix <aleixmorgadas@users.noreply.github.com>
2020-01-19 19:38:09 +0100

    Init

---
+++ src/main/java/com/github/aleixmorgadas/linkedinreactions/LinkedinreactionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleixmorgadas.linkedinreactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinreactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinreactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aleixmorgadas/linkedinreactions/LinkedinreactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aleixmorgadas.linkedinreactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinreactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
